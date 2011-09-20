9f042420a9ef03c120b9726924b92859dc258bc3
stereosteve <stereosteve@gmail.com>
2011-09-19 23:46:51 -0400

    Sheet#goods

--- lib/babelize/sheet.rb
+++ lib/babelize/sheet.rb
@@ -2,7 +2,8 @@ module Babelize
   class Sheet
 
     attr_accessor :sheet
-    def initialize()
+    def initialize(key)
+      @key = key
       @@session = nil
       @rows, @cols = [], []
       @handlers = {}
@@ -23,8 +24,27 @@ module Babelize
       @@session ||= GoogleSpreadsheet.login(ask_username, ask_password)
     end
 
-    def load_sheet(key)
-      @sheet ||= session.spreadsheet_by_key(key).worksheets[0]
+    def sheet
+      @sheet ||= session.spreadsheet_by_key(@key).worksheets[0]
+    end
+
+    def header
+      @sheet.rows[0]
+    end
+
+    def goods
+      @goods = {}
+      @sheet.rows.each_with_index do |row, row_num|
+        next if row_num == 0
+        key = row[0]
+        row.each_with_index do |col, col_num|
+          next if col_num == 0
+          local = header[col_num]
+          @goods[local] ||= {}
+          @goods[local][key] = col
+        end
+      end
+      return @goods
     end
 
   end
--- test/sheet_test.rb
+++ test/sheet_test.rb
@@ -5,9 +5,12 @@ require 'babelize'
 
 class SheetTest < MiniTest::Unit::TestCase
   def test_sheet
-    sheet = Babelize::Sheet.new
-    sheet.load_sheet('0Agc_almxTeMSdDgzWkpicHpQT09SUWFhUVloM3g4MVE')
+    sheet = Babelize::Sheet.new('0Agc_almxTeMSdDgzWkpicHpQT09SUWFhUVloM3g4MVE')
     assert sheet.sheet
     ap sheet.sheet
+
+    ap sheet.header
+    ap sheet.goods
+    puts sheet.goods.to_yaml
   end
 end
